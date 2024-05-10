067dcb5d4bf03b5cce0dae34b9718fbd4d7958d8
miguelarian <miguelvr88@gmail.com>
2024-05-10 16:31:05 +0200

    add input validation tests

--- bin-numbers/src/main/java/com/miguelvela/BINClassifier.java
+++ bin-numbers/src/main/java/com/miguelvela/BINClassifier.java
@@ -2,6 +2,12 @@ package com.miguelvela;
 
 public class BINClassifier {
 
+    private String[][] binRanges;
+
+    public BINClassifier(String[][] binRanges) {
+        this.binRanges = binRanges;
+    }
+
     public String classify(String card) {
         return null;
     }
--- bin-numbers/src/test/java/com/miguelvela/BINClassifierTest.java
+++ bin-numbers/src/test/java/com/miguelvela/BINClassifierTest.java
@@ -2,11 +2,38 @@ package com.miguelvela;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class BINClassifierTest {
 
     @Test
-    void classify() {
+    void classify_withEmptyStringCard_returnsNull() {
+        String[][] binRanges = new String[][]{};
+        BINClassifier classifier = new BINClassifier(binRanges);
+
+        String result = classifier.classify("");
+
+        assertThat("Empty String did not return null", result, is(nullValue()));
+    }
+
+    @Test
+    void classify__nullCard_returnsNull() {
+        String[][] binRanges = new String[][]{};
+        BINClassifier classifier = new BINClassifier(binRanges);
+
+        String result = classifier.classify(null);
+
+        assertThat("Null card did not return null", result, is(nullValue()));
+    }
+
+    @Test
+    void classify_withRandomStringCard_returnsNull() {
+        String[][] binRanges = new String[][]{};
+        BINClassifier classifier = new BINClassifier(binRanges);
+
+        String result = classifier.classify("this is not a card");
+
+        assertThat("A random String did not return null", result, is(nullValue()));
     }
 }
