82f14320f7d4585c2e97bb059ce351d3579ca833
miguelarian <miguelvr88@gmail.com>
2024-05-10 17:11:28 +0200

    refactor to pass visa test and validations

--- bin-numbers/src/main/java/com/miguelvela/BINClassifier.java
+++ bin-numbers/src/main/java/com/miguelvela/BINClassifier.java
@@ -1,5 +1,9 @@
 package com.miguelvela;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BINClassifier {
 
     private String[][] binRanges;
@@ -10,17 +14,40 @@ public class BINClassifier {
 
     public String classify(String card) {
 
-        if(card == null) {
+        if(card == null || card == "") {
+            return null;
+        }
+        String regex = "^\\d{4} \\d{4} \\d{4} \\d{4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(card);
+
+        if (!matcher.matches()) {
             return null;
         }
 
-        if(card.equals("4111 1111 1111 1111")) {
-            return "Visa";
-        }
-        if(card.equals("4111 2222 2222 2222")) {
-            return "Visa";
-        }
+        String[] cardSegments = card.split(" ");
+        String cardSanitized = String.join("", cardSegments);
+        long bin = Long.parseLong(cardSanitized.substring(0, 10));
 
-        return null;
+
+        return Arrays.stream(this.binRanges)
+                .map(binRange -> {
+                    String[] binRangeStartSegments = binRange[0].split(" ");
+                    String binRangeStartSanitized = String.join("", binRangeStartSegments);
+                    long binStart = Long.parseLong(binRangeStartSanitized);
+
+                    String[] binRangeEndSegments = binRange[1].split(" ");
+                    String binRangeEndSanitized = String.join("", binRangeEndSegments);
+                    long binEnd = Long.parseLong(binRangeEndSanitized);
+
+                    String schema = binRange[2];
+
+                    if(bin >= binStart && bin <= binEnd) {
+                        return schema;
+                    }
+                    return null;
+                })
+                .findFirst()
+                .orElse(null);
     }
 }
